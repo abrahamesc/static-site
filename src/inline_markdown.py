6378746ce49d343b0c748f0d9c1a837fff9e70aa
Abraham Escobedo <abraham94esc@gmail.com>
2024-05-06 19:15:52 -0500

    The image splitter is complete

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -3,8 +3,11 @@ from textnode import (
     text_type_text,
     text_type_bold,
     text_type_italic,
-    text_type_bold
+    text_type_bold,
+    text_type_image
  )
+
+from extract_markdown import extract_markdown_links, extract_markdown_images
 
 def split_nodes_delimeter(old_nodes, delimeter, text_type):
     new_nodes = []
@@ -29,8 +32,42 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
     return new_nodes
 
 def split_nodes_image(old_nodes):
-    pass
-
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            print(f'This is original_text {original_text}')
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    text_type_image,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+            print(f'This is original_text after section[1] {sections[1]}')
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes    
 
 def split_nodes_link(old_nodes):
     pass
+
+node = TextNode(
+    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+    text_type_text,
+)
+
+
--- src/textnode.py
+++ src/textnode.py
@@ -4,6 +4,8 @@ text_type_text="text"
 text_type_code="code"
 text_type_bold="bold"
 text_type_italic="italic"
+text_type_image="image"
+text_type_link="link"
 
 class TextNode():
     def __init__(self, text, type, url=None):
