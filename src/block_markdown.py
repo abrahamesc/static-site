6736901f37dd583338668d38567891a80a4c06d6
Abraham Escobedo <abraham94esc@gmail.com>
2024-05-07 13:22:53 -0500

    started work on block_to_block funcition

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,9 @@
-
+block_type_paragraph = "paragraph"
+block_type_code = "code"
+block_type_heading = "heading"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
 
 def markdown_to_blocks(markdown):
 
@@ -11,3 +16,6 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(markdown_block):
+
