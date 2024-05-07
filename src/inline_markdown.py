bc1db945dfd7e7ca6265b585f2a8011ed2f58d35
Abraham Escobedo <abraham94esc@gmail.com>
2024-05-07 07:52:36 -0500

    finished the link and image split functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import (
     TextNode,
     text_type_text,
@@ -7,7 +8,16 @@ from textnode import (
     text_type_image
  )
 
-from extract_markdown import extract_markdown_links, extract_markdown_images
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
+
 
 def split_nodes_delimeter(old_nodes, delimeter, text_type):
     new_nodes = []
@@ -30,6 +40,7 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
                 split_nodes.append(TextNode(parts[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -62,12 +73,36 @@ def split_nodes_image(old_nodes):
             new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes    
 
-def split_nodes_link(old_nodes):
-    pass
 
-node = TextNode(
-    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
-    text_type_text,
-)
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_images(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            print(f'This is original_text {original_text}')
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(
+                TextNode(
+                    link[0],
+                    text_type_image,
+                    link[1],
+                )
+            )
+            original_text = sections[1]
+            print(f'This is original_text after section[1] {sections[1]}')
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes    
 
 
