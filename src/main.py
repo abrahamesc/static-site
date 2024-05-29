2b14df3f0844cafe7ba76e434d3a694a4b6c69fe
Abraham Escobedo <abraham94esc@gmail.com>
2024-05-29 10:18:06 -0500

    Finished the recursive copying function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from textnode import TextNode
+import os, shutil
+
+def copy_tree(source, destination):
+
+    if destination == "../public/":
+        shutil.rmtree(destination)
+
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    list_of_files = os.listdir(source)
+
+    for entry in list_of_files:
+        source_path = os.path.join(source, entry)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination)
+            print(f'Copying {source_path} to {destination}')
+        elif os.path.isdir(source_path):
+            copy_tree(source_path,os.path.join(destination, entry))
+
+
+
 
 def main():
-    a = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(a)
+    print("Copying static files to public dir...")
+    copy_tree('../static/', '../public/')
 
 main()
-
